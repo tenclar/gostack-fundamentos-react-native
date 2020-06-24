891e458f4fd57bc2f9d681b4e727f2113aa61526
Tenclar <tenclarvalus@gmail.com>
2020-06-24 18:06:20 -0500

    atualiza geral

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,26 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    // usar reducer
 
-    return formatValue(0);
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
+
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+      return accumulator + productsQuantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -40,22 +40,35 @@ const Cart: React.FC = () => {
 
   function handleIncrement(id: string): void {
     // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
     // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    // usar reducer
 
-    return formatValue(0);
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
+
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+      return accumulator + productsQuantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
@@ -64,10 +77,8 @@ const Cart: React.FC = () => {
         <ProductList
           data={products}
           keyExtractor={item => item.id}
+          ListFooterComponentStyle={{ height: 80 }}
           ListFooterComponent={<View />}
-          ListFooterComponentStyle={{
-            height: 80,
-          }}
           renderItem={({ item }: { item: Product }) => (
             <Product>
               <ProductImage source={{ uri: item.image_url }} />
